950c98843ce747bd1a21b06f2545f14f71909b97
KawserAhmedpro <kawserahmedratul@gmail.com>
2026-08-11 01:54:19 +0600

    class 06 assignment Encupsulation

---
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace TwitterClone.Domain.Entities
+{
+    public class User
+    {
+        private Guid _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+
+
+        public User()
+        {
+            _id = Guid.NewGuid();
+        }
+        public Guid Id
+        {
+            get => _id;
+          
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set => _firstName = value;
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
+
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+    }
+}
