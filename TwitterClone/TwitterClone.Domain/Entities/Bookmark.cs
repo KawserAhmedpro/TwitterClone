f2e788fb681e9ffb6ca7189f0bc089ba159ae272
KawserAhmedpro <kawserahmedratul@gmail.com>
2026-08-11 23:08:50 +0600

    OOP-02-Inheretence-assignment 07

--- TwitterClone/TwitterClone.Domain/Entities/Bookmark.cs
+++ TwitterClone/TwitterClone.Domain/Entities/Bookmark.cs
@@ -1,26 +1,17 @@
 ﻿namespace TwitterClone.Domain.Entities
 {
-    public class Bookmark
+    public class Bookmark:BaseEntity
     {
-        private Guid _id;
         private Guid _userId;
         private Guid _tweetId;
-        private DateTime _createdAt;
-        private DateTime _modifiedAt;
-        private Guid _createdBy;
-        private Guid _modifiedBy;
+        
 
-        public Bookmark()
+        public Bookmark(): base(Guid.NewGuid())
         {
-            _id = Guid.NewGuid();
-            _createdAt = DateTime.UtcNow;
+            
         }
 
-        public Guid Id
-        {
-            get { return _id; }
-        }
-
+       
         public Guid UserId
         {
             get { return _userId; }
@@ -33,28 +24,7 @@
             set { _tweetId = value; }
         }
 
-        public DateTime CreatedAt
-        {
-            get { return _createdAt; }
-        }
-
-        public DateTime ModifiedAt
-        {
-            get { return _modifiedAt; }
-            set { _modifiedAt = value; }
-        }
-
-        public Guid CreatedBy
-        {
-            get { return _createdBy; }
-            set { _createdBy = value; }
-        }
-
-        public Guid ModifiedBy
-        {
-            get { return _modifiedBy; }
-            set { _modifiedBy = value; }
-        }
+        
 
     }
 }     
