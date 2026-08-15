867eace0899f163b82124fa2a09c4fc4db3fa2fe
KawserAhmedpro <kawserahmedratul@gmail.com>
2026-08-15 23:26:51 +0600

    Abstruct class assignment

--- TwitterClone/TwitterClone.Domain/Entities/FriendReqNotification.cs
+++ TwitterClone/TwitterClone.Domain/Entities/FriendReqNotification.cs
@@ -6,10 +6,15 @@ namespace TwitterClone.Domain.Entities
 {
     public class FriendReqNotification : Notification
     {
-        public FriendReqNotification(int requestedByUserId) : base("FriendRequest")
+        public FriendReqNotification(Guid requestedByUserId) : base("FriendRequest")
         {
             RequestedByUserId = requestedByUserId;
         }
-        public int RequestedByUserId { get; set; }
+        public Guid RequestedByUserId { get; set; }
+
+        public override string GetMessage()
+        {
+            return $"User with ID {RequestedByUserId} sent you a friend request.";
+        }
     }
 }
--- TwitterClone/TwitterClone.Domain/Entities/SystemNotification.cs
+++ TwitterClone/TwitterClone.Domain/Entities/SystemNotification.cs
@@ -6,13 +6,18 @@ namespace TwitterClone.Domain.Entities
 {
     public class SystemNotification : Notification
     {
-        public SystemNotification(int systemNotificationByUserId) : base("System")
+        public SystemNotification(Guid  systemNotificationByUserId) : base("System")
         {
             SystemNotificationByUserId = systemNotificationByUserId;
         }
 
         //System er ki userid ase??system notification er khetre amra ki use korbo?
         //like comment user kore tai user id korsi,but system er khetre ki korbo?/
-        public int SystemNotificationByUserId { get; set; }
+        public Guid  SystemNotificationByUserId { get; set; }
+
+        public override string GetMessage()
+        {
+            return $"System notification triggered by user with ID {SystemNotificationByUserId}.";
+        }
     }
 }
